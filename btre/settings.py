ca1f0b30b86504ef257b7109eb41bb1115a22532
sigma21 <semihkocakel@hotmail.com>
2019-12-30 15:46:43 +0100

    deploy

--- btre/settings.py
+++ btre/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')h7&kn-+fcmy3!q+frxayngb)=ifc4c=rmkp^eftbmf(l-d^s@'
+# SECRET_KEY = ')h7&kn-+fcmy3!q+frxayngb)=ifc4c=rmkp^eftbmf(l-d^s@'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ')h7&kn-+fcmy3!q+frxayngb)=ifc4c=rmkp^eftbmf(l-d^s@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -47,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,7 +135,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
-
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media Folders Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -149,3 +155,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = "semihkocakel@hotmail.com"
 EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = True
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
